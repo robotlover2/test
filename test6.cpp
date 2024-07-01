22bf8a103fad2923031043dc5cabd11f79c4ae85
ydw05 <ydw05@naver.com>
2024-07-02 08:02:30 +0900

    first upload

--- test6.cpp
+++ test6.cpp
@@ -6,18 +6,20 @@ int main(void) {
 	scanf_s("%d", &num);
 	for (int i = 0; i < num; i++)
 	{
-		for (int k = i; k < (num - 1); k++)
+		for (int k = 0; k < (num - i); k++)
 		{
 			printf(" ");
 		}
-		if (num >= 3) {
-			for (int k = 0; k <= (i * 2)-1 ; k++) {
-			
-				
+		if (i>0 && i<num-1) {
+			printf("*"); // 별표를 먼저 채우고 
+			for (int k = 0; k < i * 2 - 1; k++) {
+				printf(" "); // 중간 공백을 먼저 만들고 
 			}
+			printf("*");// 다시 별표로 채운다 
 
 		}
-		else {
+		else {// 첫줄과 마지막줄은 원래 그대로 해야함 
+
 			for (int k = 0; k <= (i * 2); k++)
 			{
 				printf("*");
